5901005a3b229828f6b1cc0693f714b40c55d651
Nito <100199931+Nitoryu008@users.noreply.github.com>
2024-11-28 18:07:23 +0900

    wip: impl header

--- panda.config.ts
+++ panda.config.ts
@@ -5,16 +5,41 @@ export default defineConfig({
   preflight: true,
 
   // Where to look for your css declarations
-  include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+  include: [
+    "./src/**/*.{js,jsx,ts,tsx,astro}",
+    "./pages/**/*.{js,jsx,ts,tsx,astro}",
+  ],
 
   // Files to exclude
   exclude: [],
 
   // Useful for theme customization
   theme: {
-    extend: {},
+    extend: {
+      tokens: {
+        fonts: {
+          metro: { value: "Metro Retro NF, serif" },
+          dots: { value: "Zen Dots, sans-serif" },
+        },
+        spacing: {
+          xs: {value: "0.5rem"},
+          sm: { value: "1rem" },
+          md: { value: "1.5rem" },
+          lg: { value: "2rem" },
+          xl: { value: "2.5rem" },
+        },
+      },
+    },
   },
 
   // The output directory for your css system
   outdir: "styled-system",
+  globalFontface: {
+    MetroRetroNF: {
+      src: "url(/fonts/MetroRetroNF.ttf) format('ttf')",
+      fontWeight: 400,
+      fontStyle: "normal",
+      fontDisplay: "swap",
+    },
+  },
 });
