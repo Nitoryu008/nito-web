ce53932faca7fe871f9959557ce1e42849e4e889
Nito <100199931+Nitoryu008@users.noreply.github.com>
2024-11-29 20:49:29 +0900

    chore: add eslint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+import { includeIgnoreFile } from "@eslint/compat";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  includeIgnoreFile(gitignorePath),
+  { ignores: ["postcss.config.cjs", "src/env.d.ts"] },
+];
