b952c84863f76d88da85f684b111067573eb833c
Nito <100199931+Nitoryu008@users.noreply.github.com>
2024-12-03 23:09:15 +0900

    feat: add BaseHead

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,6 @@
 export const DESCRIPTION =
   "プログラミングちょっとできるになりたい。最近はWebをやっています。いろんな技術を触ってみたい。";
+export const DEFAULT_SITE_DESCRIPTION = "にとの個人サイトです。" + DESCRIPTION;
+export const SITE_NAME = "NITO LAB.";
+export const TWITTER_ACCOUNT = "@nito_008";
+export const DEFAULT_OG_IMAGE = "/og_image.png";
